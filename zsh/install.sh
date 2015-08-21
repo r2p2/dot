725d4aa3c00aed6211061f04788ee82ad9abbda9
Robert Peters <robert.peters@roth-rau.com>
2015-08-21 23:59:53 +0200

    zsh: Add installation script

---
+++ zsh/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DATE=`date +%Y-%m-%d:%H:%M:%S`
+
+if [ -h ~/.zshrc ]; then
+	echo "Your zshrc is a link: removed"
+	rm  ~/.zshrc
+fi
+
+if [ -f ~/.zshrc ]; then
+	echo "Create backup of ~/.zshrc"
+	mv ~/.vimrc ~/zshrc_backup_$DATE
+fi
+
+ln -s ~/dot/zsh/zshrc ~/.zshrc
